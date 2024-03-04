bfc6f4bf289c6f31ecbb496667d0b40a6f338160
anagornaia <41295312+anagornaia@users.noreply.github.com>
2024-03-04 15:28:33 +0200

    Add files via upload

---
+++ src/main/java/com/anagornii/learning/javacodetrack/JavaCodeTrackApplication.java
@@ -0,0 +1,13 @@
+package com.anagornii.learning.javacodetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anagornii/learning/javacodetrack/JavaCodeTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anagornii.learning.javacodetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
